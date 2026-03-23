6de25962ac5ff604fd25f02f53806157ed4d8468
Gabriel-Galindo <95386248+Gabriel-Galindo@users.noreply.github.com>
2026-03-23 15:13:29 -0300

    Update Edmonds-Karp.cpp
    Mudanca de formato de matriz de adjacencia para lista de adjacencia

--- algoritimos/Grafos/Edmonds-Karp.cpp
+++ algoritimos/Grafos/Edmonds-Karp.cpp
@@ -1,27 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
-//Usa BFS
-//Necessario fonte e sumidouro
-//O(ve^2) sendo v=vertices e=arestas
-int rgrafo[][]; // alterar BFS para usar grafo residual.
-int fluxoMaximo(int ini, int fim, int tam){
+//O(Ef) E=num de arestas, f=fluxomax
+struct Edge{
+    int to;
+    int cap;
+};
+
+const int N=112,INF=0x3f3f3f3f;
+
+int vis[N],anterior[N],aresta[N];
+vector<int> g[N];
+vector<Edge> edges;
+set<int> s;
+//Funcao auxiliar para criar arestas
+void addEdge(int a,int b,int c){
+    g[a].push_back(edges.size());
+    edges.push_back(Edge{b,c});
+    g[b].push_back(edges.size());
+    edges.push_back(Edge{a,c});
+}
+//Roda BFS para pegar o caminho
+int BFS(int ini, int fim){ // 1 se tiver caminho, 0 caso nao
+    int i;
+    queue<int> fila;
+    memset(vis,0,sizeof(vis));
+    fila.push(ini); vis[ini] = 1; anterior[ini] = -1;
+    while (!fila.empty()){
+        int a=fila.front();fila.pop();
+        for(auto i:g[a]){
+            int b=edges[i].to,c=edges[i].cap;
+            if(!vis[b]&&c){
+                anterior[b]=a;
+                aresta[b]=i;
+                if(b==fim) return 1;
+                fila.push(b);
+                vis[b]=1;
+            }
+        }
+    }
+    return 0;
+}
+
+int fluxoMaximo(int ini, int fim){
     int u, v;
     int fluxo = 0;
     int bot;
-    for(u = 0; u < tam; u++)
-        for(v = 0; v < tam; v++) rgrafo[u][v]=mat[u][v];
-    
-    while (BFS(ini, fim, tam)){
+    while (BFS(ini, fim)){
         bot = INF;
         for (v = fim; v != ini; v = anterior[v]){
-            u = anterior[v];
-            bot = min(bot, rgrafo[u][v]);
+            u = aresta[v];
+            bot = min(bot, edges[u].cap);
         }
         for (v = fim; v != ini; v = anterior[v]){
-            u = anterior[v];
-            rgrafo[u][v] -= bot; rgrafo[v][u] += bot; 
+            u = aresta[v];
+            edges[u].cap -= bot; edges[u^1].cap += bot; 
         }
         fluxo += bot;
     }
     return fluxo;
+}
+//Funcao que determina arestas que participam do minCut
+int minCut(int tam){
+    for(int i=0;i<tam;i++){
+        if(vis[i]){
+            for(auto j:g[i]){
+                int b=edges[j].to;
+                if(!vis[b]){
+                    s.insert(j/2+1);
+                }
+            }
+        }
+    }
+    return s.size();
 }
